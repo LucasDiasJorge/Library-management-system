68fc920515777b530198427ae9389ea43687b239
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-03-25 15:49:55 -0300

    Create Book.cs

---
+++ lib-management/Book.cs
@@ -0,0 +1,31 @@
+﻿namespace lib_management
+{
+
+    public class Book
+    {
+        public string Title { get; }
+        public string Isbn { get; }
+        public string Author { get; }
+
+        public Book(string title, string isbn, string author)
+        {
+            Title = title;
+            Isbn = isbn;
+            Author = author;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Book other)
+            {
+                return Title.Equals(other.Title, StringComparison.OrdinalIgnoreCase);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Title.ToLower().GetHashCode();
+        }
+    }
+}
