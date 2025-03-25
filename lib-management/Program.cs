3310ab5c3d3c28f225a87ac7bda67dc2cc3e231a
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-03-25 15:08:56 -0300

    Creating looping structure

--- lib-management/Program.cs
+++ lib-management/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace lib_management;
+﻿using System;
+
+namespace lib_management;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HashSet<Book> lib = new HashSet<Book>();
+
+        while (true)
+        {
+            Console.WriteLine("Library Management System");
+            Console.WriteLine("1. Add a Book");
+            Console.WriteLine("2. Remove a Book");
+            Console.WriteLine("3. Display Library");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Adding a book...");
+                    break;
+                case "2":
+                    Console.WriteLine("Removing a book...");
+                    break;
+                case "3":
+                    Console.WriteLine("Displaying library...");
+                    break;
+                case "4":
+                    Console.WriteLine("Exiting...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
     }
 }
